c2fac17403cfcf717432aa68dd9142414acf973d
neusware <neusware@hotmail.com>
2024-04-24 20:10:07 +0200

    Commit
    Primer en desarrollo

--- JAVA PROJECT/src/App.java
+++ JAVA PROJECT/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println("Primer cambio en Desarrollo tras crear la rama a partir del primer commit");
     }
 }
