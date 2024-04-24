56e1686616765e829c468b300943401f653ad1ca
neusware <neusware@hotmail.com>
2024-04-24 20:17:27 +0200

    Commit
    Arreglo en hotfix tras crear la rama a partir del primer commit de main

--- JAVA PROJECT/src/App.java
+++ JAVA PROJECT/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println("Arreglo en hotfix 1");
     }
 }
