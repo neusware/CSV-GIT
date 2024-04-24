633bf854a490ce413be634d34207508f56bba50f
neusware <neusware@hotmail.com>
2024-04-24 20:15:42 +0200

    Commit
    Segundo en la rama FeatureTwo

--- JAVA PROJECT/src/App.java
+++ JAVA PROJECT/src/App.java
@@ -4,5 +4,6 @@ public class App {
 
         System.out.println("Primer cambio en Desarrollo tras crear la rama a partir del primer commit");
         System.out.println("Primer cambio en FeatureTwo, tras crear la rama a partir del primer commit de Desarrollo");
+        System.out.println("Segundo cambio en FeatureTwo");
     }
 }
