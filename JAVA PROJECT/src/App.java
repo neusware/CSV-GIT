fe4bcad75405c414f73046253c7004c68f4d37ed
neusware <neusware@hotmail.com>
2024-04-24 20:30:18 +0200

    Commit
    Primer commit en Release, tras crear la rama desde el ultimo commit de desarrollo

--- JAVA PROJECT/src/App.java
+++ JAVA PROJECT/src/App.java
@@ -7,5 +7,7 @@ public class App {
         System.out.println("Primer cambio en Desarrollo tras crear la rama a partir del primer commit");
         System.out.println("Primer cambio en FeatureTwo, tras crear la rama a partir del primer commit de Desarrollo");
         System.out.println("Segundo cambio en FeatureTwo");
+
+        System.out.println("Primer cambio en Release, tras crear la rama a partir del último commit de desarrollo");
     }
 }
